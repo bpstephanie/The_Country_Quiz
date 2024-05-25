f0e5c958a736022df8f0939d415df848a3c5caf7
Stephanie Bell <stephaniebellpalomino@hotmail.com>
2024-05-25 00:27:38 +0000

    Add layover to country buttons, add initial function to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function createUsername () {
+
+}
+
+function runGame() {
+
+}
+
+function howToPlay() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongScore() {
+
+}
+
+function displayHighScore() {
+
+}
+
+function playSounds() {
+    
+}
+
+function displayQuestion() {
+
+}
